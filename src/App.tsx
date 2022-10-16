00f34404b6a77abba4b8c87c63b26ab888fa47eb
JaneMoroz <moroz.jane@yahoo.com>
2022-10-16 17:28:56 +0300

    feat: add desktop navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./assets/css/style.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <h1>E-commerce product page</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import userImage from "../assets/images/image-avatar.png";
+import logo from "../assets/logo.svg";
+
+const Navbar = () => {
+  return (
+    <div className="container">
+      <nav className="navbar">
+        <a href="#" className="navbar__logo">
+          <img src={logo} alt="logo" />
+        </a>
+        <div className="navbar__links">
+          <a href="#" className="btn btn_type_text">
+            collections
+          </a>
+          <a href="#" className="btn btn_type_text">
+            men
+          </a>
+          <a href="#" className="btn btn_type_text">
+            women
+          </a>
+          <a href="#" className="btn btn_type_text">
+            about
+          </a>
+          <a href="#" className="btn btn_type_text">
+            contact
+          </a>
+        </div>
+        <div className="navbar__user">
+          <button type="button" className="btn btn_type_icon-big">
+            <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
+                fill="#69707D"
+                fill-rule="nonzero"
+              />
+            </svg>
+          </button>
+          <a href="#" className="btn btn_type_img">
+            <img src={userImage} alt="user" />
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
