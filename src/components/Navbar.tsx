137bea25e6b83b89fe79c8b3b4e3e5dbbfe0da5f
JaneMoroz <moroz.jane@yahoo.com>
2022-10-16 21:40:34 +0300

    feat: add mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,10 +6,33 @@ const Navbar = () => {
   return (
     <div className="container">
       <nav className="navbar">
-        <a href="#" className="navbar__logo">
-          <img src={logo} alt="logo" />
-        </a>
+        <div className="navbar__menu">
+          <button
+            type="button"
+            className="navbar__menu_btn btn btn_type_icon-big"
+          >
+            <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M16 12v3H0v-3h16Zm0-6v3H0V6h16Zm0-6v3H0V0h16Z"
+                fill="#69707D"
+                fill-rule="evenodd"
+              />
+            </svg>
+          </button>
+          <a href="#">
+            <img src={logo} alt="logo" />
+          </a>
+        </div>
         <div className="navbar__links">
+          <button type="button" className="btn btn_type_icon-big">
+            <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+                fill="#69707D"
+                fill-rule="evenodd"
+              />
+            </svg>
+          </button>
           <a href="#" className="btn btn_type_text">
             collections
           </a>
@@ -41,6 +64,7 @@ const Navbar = () => {
           </a>
         </div>
       </nav>
+      <div className="overlay"></div>
     </div>
   );
 };
