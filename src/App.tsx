dfec6a0aa4a9a1b65cbe98acd3167ffc364cf328
JaneMoroz <moroz.jane@yahoo.com>
2022-10-18 16:51:27 +0300

    feat: add product

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./assets/css/style.css";
 import Navbar from "./components/Navbar";
+import Product from "./components/Product";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Product />
     </>
   );
 }
